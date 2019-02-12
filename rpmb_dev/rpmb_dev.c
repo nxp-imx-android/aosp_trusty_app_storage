666c9abeaf36960da867871f9e7b4ab29bdd3d8b
Matthew Maurer <mmaurer@google.com>
2019-02-11 23:20:48 -0800

    [rpmb_dev] Convert rpmb_dev to a daemon
    Previously, rpmb_dev would take a file on the command line to read a
    single complete request, and write a single complete response to.
    This function has been replaced by operating as a daemon on a unix
    domain socket at a provided path.
    
    The --init functionality is unchanged and not daemonized.
    
    Bug: 124277696
    Change-Id: I83a831988ce5ae9d9874f0da8a9688f8a8c46b9c

--- rpmb_dev/rpmb_dev.c
+++ rpmb_dev/rpmb_dev.c
@@ -33,7 +33,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/socket.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/un.h>
 #include <unistd.h>
 
 #if OPENSSL_VERSION_NUMBER < 0x10100000L
@@ -72,7 +75,7 @@ struct rpmb_dev_state {
     struct rpmb_data_header header;
     struct rpmb_packet cmd[MAX_PACKET_COUNT];
     struct rpmb_packet res[MAX_PACKET_COUNT];
-    int cmd_count;
+    uint16_t cmd_count;
     uint16_t res_count;
     int data_fd;
 };
@@ -402,8 +405,103 @@ err:
     }
 }
 
+/*
+ * Receives data until one of the following is true:
+ * - The buffer is full (return will be len)
+ * - The connection closed (return > 0, < len)
+ * - An error occurred (return will be the negative error code from recv)
+ */
+ssize_t recv_until(int sock, void* dest, size_t len) {
+    size_t bytes_recvd = 0;
+    while (bytes_recvd < len) {
+        ssize_t ret = recv(sock, dest, len - bytes_recvd, 0);
+        if (ret < 0) {
+            return ret;
+        }
+        dest += ret;
+        bytes_recvd += ret;
+        if (ret == 0) {
+            break;
+        }
+    }
+    return bytes_recvd;
+}
+
+/*
+ * Handles an incoming connection to the rpmb daemon.
+ * Returns 0 if the client disconnects without violating the protocol.
+ * Returns a negative value if we terminated the connection abnormally.
+ *
+ * Arguments:
+ *   conn_sock - an fd to send/recv on
+ *   s - an initialized rpmb device
+ */
+int handle_conn(struct rpmb_dev_state* s, int conn_sock) {
+    int ret;
+
+    while (true) {
+        s->res_count = ~0;
+        memset(s->res, 0, sizeof(s->res));
+        ret = recv_until(conn_sock, &s->res_count, sizeof(s->res_count));
+
+        /*
+         * Disconnected while not in the middle of anything.
+         */
+        if (ret <= 0) {
+            return 0;
+        }
+
+        if (s->res_count > MAX_PACKET_COUNT) {
+            fprintf(stderr, "rpmb_dev: Recieve count too large: %d\n",
+                    s->res_count);
+            return -1;
+        }
+        if (s->res_count <= 0) {
+            fprintf(stderr, "rpmb_dev: Recieve count too small: %d\n",
+                    s->res_count);
+            return -1;
+        }
+
+        ret = recv_until(conn_sock, &s->cmd_count, sizeof(s->cmd_count));
+        if (ret != sizeof(s->cmd_count)) {
+            fprintf(stderr, "rpmb_dev: Failed to read cmd_count");
+            return -1;
+        }
+
+        if (s->cmd_count == 0) {
+            fprintf(stderr, "rpmb_dev: Must contain at least one command\n");
+            return -1;
+        }
+
+        if (s->cmd_count > MAX_PACKET_COUNT) {
+            fprintf(stderr, "rpmb_dev: Command count is too large\n");
+            return -1;
+        }
+
+        size_t cmd_size = s->cmd_count * sizeof(s->cmd[0]);
+        ret = recv_until(conn_sock, s->cmd, cmd_size);
+        if (ret != (int)cmd_size) {
+            fprintf(stderr,
+                    "rpmb_dev: Failed to read command: "
+                    "cmd_size: %zu ret: %d, %s\n",
+                    cmd_size, ret, strerror(errno));
+            return -1;
+        }
+
+        rpmb_dev_process_cmd(s);
+
+        size_t resp_size = sizeof(s->res[0]) * s->res_count;
+        ret = send(conn_sock, s->res, resp_size, 0);
+        if (ret != (int)resp_size) {
+            fprintf(stderr, "rpmb_dev: Failed to send response: %d, %s\n", ret,
+                    strerror(errno));
+            return -1;
+        }
+    }
+}
+
 void usage(const char* argv0) {
-    fprintf(stderr, "Usage: %s [-d|--dev] <datafile> [-c|--cmd] <cmdresfile>\n",
+    fprintf(stderr, "Usage: %s [-d|--dev] <datafile> [--sock] <socket_path>\n",
             argv0);
     fprintf(stderr,
             "or:    %s [-d|--dev] <datafile> [--size <size>] [--key key]\n",
@@ -413,16 +511,17 @@ void usage(const char* argv0) {
 int main(int argc, char** argv) {
     struct rpmb_dev_state s;
     int ret;
-    int cmdres_fd;
+    int cmdres_sock;
+    struct sockaddr_un cmdres_sockaddr;
     const char* data_file_name = NULL;
-    const char* cmdres_file_name = NULL;
+    const char* socket_path = NULL;
     int open_flags;
     int init = false;
 
     struct option long_options[] = {{"size", required_argument, 0, 0},
                                     {"key", required_argument, 0, 0},
+                                    {"sock", required_argument, 0, 0},
                                     {"dev", required_argument, 0, 'd'},
-                                    {"cmd", required_argument, 0, 'c'},
                                     {"init", no_argument, &init, true},
                                     {0, 0, 0, 0}};
 
@@ -431,17 +530,20 @@ int main(int argc, char** argv) {
     while (1) {
         int c;
         int option_index = 0;
-        c = getopt_long(argc, argv, "c:d:", long_options, &option_index);
+        c = getopt_long(argc, argv, "d:", long_options, &option_index);
         if (c == -1) {
             break;
         }
 
         switch (c) {
+        /* long args */
         case 0:
             switch (option_index) {
+            /* size */
             case 0:
                 s.header.max_block = atoi(optarg) - 1;
                 break;
+            /* key */
             case 1:
                 for (size_t i = 0; i < sizeof(s.header.key.byte); i++) {
                     if (!optarg) {
@@ -451,20 +553,27 @@ int main(int argc, char** argv) {
                     s.header.key_programmed = 1;
                 }
                 break;
+            /* sock */
+            case 2:
+                socket_path = optarg;
+                break;
             }
             break;
+        /* dev */
         case 'd':
             data_file_name = optarg;
-            break;
-        case 'c':
-            cmdres_file_name = optarg;
             break;
         default:
             usage(argv[0]);
             return EXIT_FAILURE;
         }
     }
-    if (!data_file_name || (!init == !cmdres_file_name)) {
+
+    /*
+     * We always need a data file, and at exactly one of --init or --sock
+     * must be specified.
+     */
+    if (!data_file_name || (!init == !socket_path)) {
         usage(argv[0]);
         return EXIT_FAILURE;
     }
@@ -502,42 +611,46 @@ int main(int argc, char** argv) {
         return EXIT_FAILURE;
     }
 
-    cmdres_fd = open(cmdres_file_name, O_RDWR);
-    if (cmdres_fd < 0) {
+    cmdres_sock = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (cmdres_sock < 0) {
         fprintf(stderr,
-                "rpmb_dev: Failed to open command/response file, %s: %s\n",
-                cmdres_file_name, strerror(errno));
-        return EXIT_FAILURE;
-    }
-
-    s.res_count = ~0;
-    memset(s.res, 0, sizeof(s.res));
-    ret = read(cmdres_fd, &s.res_count, sizeof(s.res_count));
-    if (s.res_count > MAX_PACKET_COUNT) {
-        fprintf(stderr, "rpmb_dev: Read count too large: %d\n", s.res_count);
-        return EXIT_FAILURE;
-    }
-    if (s.res_count <= 0) {
-        fprintf(stderr, "rpmb_dev: Read count too small: %d\n", s.res_count);
-        return EXIT_FAILURE;
-    }
-
-    ret = read(cmdres_fd, s.cmd, sizeof(s.cmd));
-    if (ret <= 0 || ret % sizeof(s.cmd[0]) != 0) {
-        fprintf(stderr, "rpmb_dev: Failed to read command file: %d, %s\n", ret,
+                "rpmb_dev: Failed to create command/response socket: %s\n",
                 strerror(errno));
         return EXIT_FAILURE;
     }
-    s.cmd_count = ret / sizeof(s.cmd[0]);
 
-    rpmb_dev_process_cmd(&s);
+    cmdres_sockaddr.sun_family = AF_UNIX;
+    strncpy(cmdres_sockaddr.sun_path, socket_path,
+            sizeof(cmdres_sockaddr.sun_path));
 
-    ret = write(cmdres_fd, s.res, sizeof(s.res[0]) * s.res_count);
-    if (ret != (int)sizeof(s.res[0]) * s.res_count) {
-        fprintf(stderr, "rpmb_dev: Failed to write response file: %d, %s\n",
-                ret, strerror(errno));
+    ret = bind(cmdres_sock, (struct sockaddr*)&cmdres_sockaddr,
+               sizeof(struct sockaddr_un));
+    if (ret < 0) {
+        fprintf(stderr,
+                "rpmb_dev: Failed to bind command/response socket: %s: %s\n",
+                socket_path, strerror(errno));
         return EXIT_FAILURE;
     }
 
-    return EXIT_SUCCESS;
+    ret = listen(cmdres_sock, 1);
+    if (ret < 0) {
+        fprintf(stderr,
+                "rpmb_dev: Failed to listen on command/response socket: %s\n",
+                strerror(errno));
+        return EXIT_FAILURE;
+    }
+
+    while (true) {
+        int conn_sock = accept(cmdres_sock, NULL, NULL);
+        if (conn_sock < 0) {
+            fprintf(stderr, "rpmb_dev: Could not accept connection: %s\n",
+                    strerror(errno));
+            return EXIT_FAILURE;
+        }
+        ret = handle_conn(&s, conn_sock);
+        close(conn_sock);
+        if (ret) {
+            fprintf(stderr, "rpmb_dev: Connection terminated: %d", ret);
+        }
+    }
 }
